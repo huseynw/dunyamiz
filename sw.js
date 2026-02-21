1931909c1820910c4c0e2f45ede145bb37d8808e
huseynw <huseynmemmedov9272@gmail.com>
2026-02-22 00:35:18 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,38 @@
-const cacheName = 'bizim-dunya-v1';
+const cacheName = 'bizim-dunya-v2';
 const assets = [
   '/',
   '/index.html',
   '/hcstil.css',
   '/hcayar.js',
-  '/manifest.json',
-  '/assets/192.png',
-  '/assets/512.png'
+  '/manifest.json'
 ];
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
-      cache.addAll(assets);
+      console.log('Fayllar kəşlənir...');
+      return cache.addAll(assets);
+    })
+  );
+  self.skipWaiting(); 
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== cacheName)
+        .map(key => caches.delete(key))
+      );
     })
   );
 });
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+      return response || fetch(e.request).catch(() => {
+        if (e.request.mode === 'navigate') {
+          return caches.match('/index.html');
+        }
+      });
     })
   );
 });
