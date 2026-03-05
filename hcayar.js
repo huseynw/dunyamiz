65345fa8906922e16b69607ddafaba3836d83d43
huseynw <huseynmemmedov9272@gmail.com>
2026-03-06 01:05:49 +0400

    Admin Panel: update_config

--- hcayar.js
+++ hcayar.js
@@ -1,4 +1,4 @@
-const targetDate = new Date("2026-03-09T13:00:00"); 
+const targetDate = new Date("2026-03-08T13:00:00"); 
 const config = {
     githubUsername: "huseynw", 
     repoName: "dunyamiz",              
