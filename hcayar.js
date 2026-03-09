1375293697bf3548c49eabde4817dd3a8b7e2597
huseynw <huseynmemmedov9272@gmail.com>
2026-03-09 16:00:11 +0400

    Admin Panel: update_config

--- hcayar.js
+++ hcayar.js
@@ -4,7 +4,7 @@ const config = {
     repoName: "dunyamiz",              
     firstMeetingDate: "2025-10-22T00:00:00",
     startDate: "2025-08-03T00:00:00", 
-    meetingCount: 91,    
+    meetingCount: 92,    
     musicTitle: "Gözlərin dəydi gözümə"
 };
 (function() {
